f5aca7d7cb9cc5ad2c62203e048bb747f1b5b8b7
Nisha3g <nisha.3ginfo@gmail.com>
2016-03-08 17:02:15 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,4 +1,4 @@
 <?php
 
-	define('SHOPIFY_APP_API_KEY', '');
-	define('SHOPIFY_APP_SHARED_SECRET', '');
+	define('SHOPIFY_APP_API_KEY', '610b2234c094a6f2a7fcb0ed9fd0e0fd');
+	define('SHOPIFY_APP_SHARED_SECRET', '638c43a558d53064f1e297a7a9557efb');
